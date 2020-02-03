bfe1633c65b74208f6a2778df3287c3f2b7b0e73
Lucas Costa <lucas.gomes.costa@gmail.com>
2020-02-02 18:37:41 -0600

    moved app from portfolio to here, making 2 api calls

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const [doggo, setDoggo] = useState(null)
+  const [love, setLove] = useState(null)
+  const [dogs, setDogs] = useState([])
+
+  useEffect(() => {
+    axios.get('https://dog.ceo/api/breeds/image/random')
+        .then((response) => {
+          console.log('sasha.data.message: ',  response.data.message)
+          setDoggo(response.data.message)
+        })
+        .catch((error) => {
+          console.log('Error fetching dog picture: ', error)
+        })
+
+    const fname = 'Lucas'
+    const sname = 'Sasha'
+
+    axios.get(`https://love-calculator.p.rapidapi.com/getPercentage?fname=${fname}&sname=${sname}`,
+        {
+          headers: {
+            "x-rapidapi-host": "love-calculator.p.rapidapi.com",
+            "x-rapidapi-key": "dc4c55f586msh375fc415a7750c3p163853jsnba29855ee4cb",
+          }
+        }).then(response => {
+      console.log('response: ', response.data)
+      setLove(response.data)
+    }).catch(err => {
+      console.log('err: ', err)
+    })
+
+    axios.get('https://dog-spring-service.herokuapp.com/dog/all')
+        .then(res => {
+          console.log('res: ', res)
+          setDogs(res.data)
+        })
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <p>Hello</p>
+        <img src={doggo} alt="doggo" height={300} />
+        {love && (
+            <div>
+              <p>Love result: {love.result}</p>
+              <p>Love percentage: {love.percentage}</p>
+            </div>
+        )}
+        <div>Dog service response:</div>
+        {dogs.length > 0 && dogs.map(dog => (
+            <div key={dog.id}>
+                <p>Dog name: {dog.name}</p>
+                <p>Dog color: {dog.color}</p>
+            </div>
+        ))}
+      </div>
   );
 }
 
