115a62d7a24b43f75c9c5f1d38d9dd4f82a97a4b
Lucas Costa <lucas.gomes.costa@gmail.com>
2020-02-03 18:34:43 -0600

    removed api call to love stuff

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import axios from 'axios'
 
 const App = () => {
   const [doggo, setDoggo] = useState(null)
-  const [love, setLove] = useState(null)
   const [dogs, setDogs] = useState([])
 
   useEffect(() => {
@@ -14,19 +13,6 @@ const App = () => {
         })
         .catch((error) => {
           console.log('Error fetching dog picture: ', error)
-        })
-
-    const fname = 'Lucas'
-    const sname = 'Sasha'
-
-    axios.get(`https://love-calculator.p.rapidapi.com/getPercentage?fname=${fname}&sname=${sname}`,
-        {
-          headers: {
-            "x-rapidapi-host": "love-calculator.p.rapidapi.com",
-            "x-rapidapi-key": "dc4c55f586msh375fc415a7750c3p163853jsnba29855ee4cb",
-          }
-        }).then(response => {
-          setLove(response.data)
         })
 
     const gatewayUrl = process.env.REACT_APP_GATEWAY_URL
@@ -45,12 +31,6 @@ const App = () => {
       <div className="App">
         <p>Hello</p>
         <img src={doggo} alt="doggo" height={300} />
-        {love && (
-            <div>
-              <p>Love result: {love.result}</p>
-              <p>Love percentage: {love.percentage}</p>
-            </div>
-        )}
         <div>Dog service response:</div>
         {dogs.length > 0 && dogs.map(dog => (
             <div key={dog.id}>
