99ac2bc206eae0e339fa19da0b415698bdc23f35
Lucas Costa <lucas.gomes.costa@gmail.com>
2020-02-22 17:50:35 -0600

    added better notes about eslint, and prettified dog-app

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import axios from 'axios'
+import axios from 'axios';
 
 const App = () => {
-    const [doggo, setDoggo] = useState(null)
-    const [dogs, setDogs] = useState([])
+  const [doggo, setDoggo] = useState(null);
+  const [dogs, setDogs] = useState([]);
 
-    useEffect(() => {
-        axios.get('https://dog.ceo/api/breeds/image/random')
-            .then((response) => {
-                setDoggo(response.data.message)
-            })
-            .catch((error) => {
-                console.log('Error fetching dog picture: ', error)
-            })
+  useEffect(() => {
+    axios
+      .get('https://dog.ceo/api/breeds/image/random')
+      .then(response => {
+        setDoggo(response.data.message);
+      })
+      .catch(error => {
+        console.log('Error fetching dog picture: ', error);
+      });
 
-        const gatewayUrl = process.env.REACT_APP_GATEWAY_URL
+    const gatewayUrl = process.env.REACT_APP_GATEWAY_URL;
 
-        console.log('gatewayUrl: ', gatewayUrl)
-        console.log('process.env.NODE_ENV: ', process.env.NODE_ENV)
+    console.log('gatewayUrl: ', gatewayUrl);
+    console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
 
-        axios.get(`${gatewayUrl}/api/dog/all`)
-            .then(res => {
-                setDogs(res.data)
-            })
+    axios.get(`${gatewayUrl}/api/dog/all`).then(res => {
+      setDogs(res.data);
+    });
+  }, []);
 
-    }, [])
-
-    return (
-        <div className="App">
-            <p>Hello</p>
-            <img src={doggo} alt="doggo" height={300}/>
-            <div>Dog service response:</div>
-            {dogs.length > 0 && dogs.map(dog => (
-                <div key={dog.id}>
-                    <p>Dog name: {dog.name}</p>
-                    <p>Dog color: {dog.color}</p>
-                </div>
-            ))}
-        </div>
-    );
-}
+  return (
+    <div className="App">
+      <p>Hello</p>
+      <img src={doggo} alt="doggo" height={300} />
+      <div>Dog service response:</div>
+      {dogs.length > 0 &&
+        dogs.map(dog => (
+          <div key={dog.id}>
+            <p>Dog name: {dog.name}</p>
+            <p>Dog color: {dog.color}</p>
+          </div>
+        ))}
+    </div>
+  );
+};
 
 export default App;
