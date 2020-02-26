e249ef851d141161bc895fa0ac5373e7d2369ec0
Lucas Costa <lucas_costa@unigroup.com>
2020-02-26 17:29:44 -0600

    added enzyme and made test pass

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import {shallow} from "enzyme";
 import App from './App'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
-})
+describe('<App>', () => {
+  it('says hello', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('p').childAt(0).text()).toEqual('Hello')
+  });
+});
