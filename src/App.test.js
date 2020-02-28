c3d74b24e1c3f6644bcd9cf22632e791d473989e
Lucas Costa <lucas.gomes.costa@gmail.com>
2020-02-27 19:54:35 -0600

    minor changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,15 @@
 import React from 'react'
-import {shallow} from "enzyme";
+import { shallow } from 'enzyme'
 import App from './App'
 
 describe('<App>', () => {
   it('says hello', () => {
     const wrapper = shallow(<App />)
-    expect(wrapper.find('p').childAt(0).text()).toEqual('Hello')
-  });
-});
+    expect(
+      wrapper
+        .find('p')
+        .childAt(0)
+        .text(),
+    ).toEqual('Hello')
+  })
+})
